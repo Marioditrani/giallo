d9c9da2868946c64646c690ff4c51b78e96c7804
Marioditrani <marioditrani2003@gmail.com>
2023-11-29 18:12:21 +0100

    fc

---
+++ script.js
@@ -0,0 +1,36 @@
+var n_img = 5;
+var corrente = 1;
+
+function succ() {
+    corrente++;
+    if (corrente > n_img) {
+        corrente = 1;
+    }
+
+    for (var i = n_img; i > 0; i--) {
+        document.querySelector("#img_slider img:nth-child(" + i + ")").style.display = "none";
+    }
+    document.querySelector("#img_slider img:nth-child(" + corrente + ")").style.display = "block";
+}
+
+function prec() {
+    corrente--;
+    if (corrente == 0) {
+        corrente = n_img;
+    }
+
+    for (var i = n_img; i > 0; i--) {
+        document.querySelector("#img_slider img:nth-child(" + i + ")").style.display = "none";
+    }
+    document.querySelector("#img_slider img:nth-child(" + corrente + ")").style.display = "block";
+}
+
+setInterval(succ, 5000);
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
